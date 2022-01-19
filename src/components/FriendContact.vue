c8538f818a31101e94c8f98ff17048a59235f38f
Renas Barış Özkal <barisozkal11@gmail.com>
2022-01-19 16:14:46 +0300

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone: {{ phoneNumber }}</strong>
+      </li>
+      <li>
+        <strong>Email: {{ emailAddress }}</strong>
+      </li>
+    </ul>
+     <button @click="$emit('delete-friend',id);">
+       Delete Friend
+    </button>
+  </li>
+</template>
+<script>
+export default {
+
+  props: {
+    id:{
+      type:String,
+      required:true
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    phoneNumber: {
+        type:Number,
+        required:true,
+    },
+    emailAddress: {
+        type:String,
+        required:true,
+    },
+ 
+
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  emits:['delete-friend'],
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  
+  },
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,46 @@
+<template>
+  <form @submit.prevent="submitContact">
+    <div>
+      <label>Name : </label>
+      <input v-model="enteredName" type="text" />
+
+    </div>
+    <div>
+      <label>Phone : </label>
+      <input v-model="enteredPhone" type="tel" />
+
+    </div>
+    <div>
+      <label>Email : </label>
+      <input v-model="enteredEmail" type="email" />
+ 
+    </div>
+    <div>
+        <button>Add Contact</button>
+    </div>
+    
+  </form>
+</template>
+
+<script>
+export default {
+
+    emits:['add-new'],
+    data(){
+        return{
+            enteredName:'',
+            enteredPhone:'',
+            enteredEmail:''
+        }
+    },
+    methods:{
+        submitContact(){
+            this.$emit('add-new',this.enteredName,this.enteredPhone,this.enteredEmail);
+            this.enteredName='';
+            this.enteredPhone='';
+            this.enteredEmail='';
+        },
+        
+    }
+};
+</script>
